02bade9102fa40cef448bf8c9f1681d07cb4b817
Zhe-Han Mo <139523057+Mo-ZheHan@users.noreply.github.com>
2025-05-31 19:42:20 +0800

    Revert and give up rigid dynamics
    This reverts commit afdf83f43155fb9e3d976c2576302981d58d6a49.

--- scenes/IISPH_demo.py
+++ scenes/IISPH_demo.py
@@ -20,7 +20,7 @@ parser.add_argument(
     help="Path to the output USD file.",
 )
 parser.add_argument(
-    "--num_frames", type=int, default=3000, help="Total number of frames."
+    "--num_frames", type=int, default=20000, help="Total number of frames."
 )
 parser.add_argument(
     "--preview",
